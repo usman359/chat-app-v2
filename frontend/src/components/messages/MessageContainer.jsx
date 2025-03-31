a37b8b9358565b69f8db406489c64fdb18a5724f
usman359 <m.usman095@gmail.com>
2025-03-31 22:04:32 +0500

    2nd part completed - ui design

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Messages from "./Messages";
+import MessageInput from "./MessageInput";
+import { MessageSquare } from "lucide-react";
+
+const MessageContainer = () => {
+  const noChatSelected = true;
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          {/* Header */}
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">
+              To: <span className="text-gray-900 font-bold">John Doe</span>
+            </span>
+          </div>
+
+          <Messages />
+          <MessageInput />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MessageContainer;
+
+const NoChatSelected = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <p>Welcome 👋 John Doe ❄️</p>
+        <p>Select a chat to start messaging</p>
+        <MessageSquare className="size-12 md:size-20 text-center" />
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LogOut } from "lucide-react";
+
+const LogoutButton = () => {
+  return (
+    <div className="mt-auto">
+      <LogOut className="w-6 h-6 text-white cursor-pointer" />
+    </div>
+  );
+};
+
+export default LogoutButton;
