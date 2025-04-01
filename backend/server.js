acbc1ed160fe0ee506a982306bf803a0cfe61d84
usman359 <m.usman095@gmail.com>
2025-04-01 22:35:12 +0500

    5th part completed - building the final application

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,13 @@ import userRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/db.js";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -17,6 +20,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectDB();
